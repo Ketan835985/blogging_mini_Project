704bfc931222152e393a7a82e9880bdca1d076bb
Ketan Gupta <guptaketan6375@gmail.com>
2023-06-02 02:56:40 +0530

    Check

--- src/middleWare/middleWare.js
+++ src/middleWare/middleWare.js
@@ -21,6 +21,8 @@ const authenticationFun = async (req, res, next) => {
 const authorHandel = async (req, res, next) => {
     try {
         const authorId = req.body.authorId
+        const token = req.headers['x-api-key']
+        if (!token) return res.status(401).send({ status: false, message: 'Please provide token' })
         if (!authorId) {
             return res.status(400).send({ status: false, message: "Author id is missing" })
         } else {
@@ -45,6 +47,8 @@ const blogHandel = async (req, res, next) => {
     try {
         const blogId = req.params.blogId
         const authorId = req.authorId
+        const token = req.headers['x-api-key']
+        if (!token) return res.status(401).send({ status: false, message: 'Please provide token' })
         if (!mongoose.Types.ObjectId.isValid(blogId)) return res.status(400).send({ status: false, message: ' blog id is not valid' })
         const blog = await blogModel.findById(blogId)
         if (!blog || blog === null) {
