884ae279bce978cb6f5494cc26d869ab5c00f2be
Ketan Gupta <guptaketan6375@gmail.com>
2023-06-02 00:56:52 +0530

    Done

--- src/controller/authorController.js
+++ src/controller/authorController.js
@@ -2,7 +2,7 @@ const { titleCheck } = require('../middleWare/validator')
 const authorModel = require('../models/authorModel')
 const validator = require('validator')
 const jwt = require('jsonwebtoken')
-const {SECRETE_KEY} = require('../../config')
+const { SECRETE_KEY } = require('../../config')
 
 
 const createAuthor = async (req, res) => {
@@ -11,32 +11,35 @@ const createAuthor = async (req, res) => {
         if (!title || !fname || !lname || !email || !password) return res.status(400).send({ status: false, message: 'please Provide all the fields' })
         if (!titleCheck(title)) res.status(400).send({ status: false, message: `Provide a valid title ` })
         if (!validator.isEmail(email)) res.status(400).send({ status: false, message: `${email} is not valid` })
-        const author = await authorModel.create(req.body)
-        res.status(201).send({ status: true, data: author })
-
-    } catch (error) {
-        res.status(500).send({ status: false, message: error.message })
-    }
-}
-
-
-const authorLogin = async (req,res) => {
-    try {
-        const {email, password} = req.body
-        if(!email || !password) return res.status(400).send({ status: false , message: 'Please provide email and password' })
-        if(! validator.isEmail(email)) res.status(400).send({ status: false, message: `${email} is not valid` })
-        const author = await authorModel.findOne({ email: email , password: password})
-        if(!author) return res.status(401).send({ status: false, message: 'Invalid email or password' })
-        else{
-            const token = jwt.sign({
-                authorId: author._id,
-            }, SECRETE_KEY)
-            res.setHeaders('x-api-key', token)
-            res.status(200).send({status:true, data: {token:token}})
+        else {
+            const aut = await authorModel.findOne({ email: email })
+            if(aut) res.status(400).send({ status: false, message: `${email} is already registered with this account` })
+            const author = await authorModel.create(req.body)
+            res.status(201).send({ status: true, data: author })
         }
     } catch (error) {
         res.status(500).send({ status: false, message: error.message })
     }
 }
 
-module.exports = {createAuthor, authorLogin}
+
+const authorLogin = async (req, res) => {
+    try {
+        const { email, password } = req.body
+        if (!email || !password) return res.status(400).send({ status: false, message: 'Please provide email and password' })
+        if (!validator.isEmail(email)) res.status(400).send({ status: false, message: `${email} is not valid` })
+        const author = await authorModel.findOne({ email: email, password: password })
+        if (!author|| author == null) return res.status(401).send({ status: false, message: 'Invalid email or password' })
+        else {
+            const token = jwt.sign({
+                authorId: author._id,
+            }, SECRETE_KEY)
+            res.setHeaders('x-api-key', token)
+            res.status(200).send({ status: true, data: { token: token } })
+        }
+    } catch (error) {
+        res.status(500).send({ status: false, message: error.message })
+    }
+}
+
+module.exports = { createAuthor, authorLogin }
--- src/middleWare/middleWare.js
+++ src/middleWare/middleWare.js
@@ -5,13 +5,13 @@ const {SECRETE_KEY} = require('../../config')
 
 const authenticationFun = async (req, res, next) => {
     try {
-        const token = req.headers['x-api-key']
+        const token = req.headers('x-api-key')
         if (!token) return res.status(401).send({ status: false, message: 'Please provide token' })
         const decoded = jwt.verify(token, SECRETE_KEY)
         req.authorId = decoded.authorId
         next()
     } catch (error) {
-        res.status(401).send({ status: false, message: error.message })
+        res.status(404).send({ status: false, message: error.message })
     }
 }
 
